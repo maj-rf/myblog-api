f94555cf3cfed0c385d9d62754325e2f1b004788
bananabread08 <rmkfermin@gmail.com>
2022-11-09 11:10:42 +0800

    import passportConfig

--- app.js
+++ app.js
@@ -5,28 +5,46 @@ const logger = require('morgan');
 const dotenv = require('dotenv').config();
 
 const indexRouter = require('./routes/index');
-const authRouter = require('./routes/auth');
+const apiRouter = require('./routes/api');
 
 const app = express();
 const mongoInitialize = require('./config/mongoConfig');
+const passportInitialize = require('./config/passportConfig');
 
 // MongoDB
 const mongoose = require('mongoose');
 mongoInitialize(mongoose);
 
+// Passport + JWT
+passportInitialize();
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/api/auth', authRouter);
+app.use('/api', apiRouter);
 
 console.log('hello');
-
 app.listen('3001', () => {
   console.log('Server is running at port: 3000');
+});
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.json('404 not found');
 });
 
 module.exports = app;
