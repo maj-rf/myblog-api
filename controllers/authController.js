8c3b8f322792a1537454291ed9fe0a1acedbe154
bananabread08 <rmkfermin@gmail.com>
2022-11-09 15:47:10 +0800

    fix sign-up POST controller to properly save user in mongoDB

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,11 @@
 const jwt = require('jsonwebtoken');
 const passport = require('passport');
+const bcrypt = require('bcrypt');
 const { body, validationResult } = require('express-validator');
+const User = require('../models/User');
 
 // User Signup POST
-exports.signup = [
+exports.post_signup = [
   body('username', 'Username is required')
     .trim()
     .escape()
@@ -46,23 +48,29 @@ exports.signup = [
     .withMessage('Passwords do not match'),
 
   async (req, res, next) => {
-    passport.authenticate('signup', { session: false }, (err, user, info) => {
-      const errors = validationResult(req);
-      if (!errors.isEmpty()) {
-        return res.json({
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors });
+    } else {
+      bcrypt.hash(req.body.password, 10, (err, hashedPassword) => {
+        if (err) return next(err);
+        const newUser = new User({
           username: req.body.username,
-          errors: errors.array(),
+          email: req.body.email,
+          password: hashedPassword,
         });
-      }
-      if (err) return next(err);
-      res.json({ message: 'Successful account registration!', user: req.user });
-    })(req, res, next);
+        newUser.save((err) => {
+          if (err) return next(err);
+          res.status(200).json({ message: 'Account created!' });
+        });
+      });
+    }
   },
 ];
 
 // User Login POST
-exports.login = (req, res, next) => {
-  passport.authenticate('login', { session: false }, (err, user, info) => {
+exports.post_login = (req, res, next) => {
+  passport.authenticate('local', { session: false }, (err, user, info) => {
     try {
       if (err || !user) {
         return res.status(400).json({
@@ -91,7 +99,7 @@ exports.login = (req, res, next) => {
   })(req, res);
 };
 
-exports.logout = function (req, res) {
+exports.post_logout = function (req, res) {
   req.logout;
   res.redirect('/');
 };
