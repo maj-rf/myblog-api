c6fd07268f582a3f42b1ebf96bd184491e268ee7
bananabread08 <rmkfermin@gmail.com>
2022-11-10 17:51:44 +0800

    rearrange middlewares and mongo + passport order

--- app.js
+++ app.js
@@ -12,6 +12,11 @@ const app = express();
 const mongoInitialize = require('./config/mongoConfig');
 const passportInitialize = require('./config/passportConfig');
 
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
 // MongoDB
 const mongoose = require('mongoose');
 mongoInitialize(mongoose);
@@ -20,28 +25,7 @@ mongoInitialize(mongoose);
 passportInitialize(passport);
 app.use(passport.initialize());
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-
 app.use('/', indexRouter);
 app.use('/api', apiRouter);
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.json('404 not found');
-});
 
 module.exports = app;
