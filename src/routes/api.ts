4abfd47dbb9695c576a231dba3fe852a0bd5d0d5
bananabread08 <rmkfermin@gmail.com>
2022-12-11 09:48:29 +0800

    create private route for user and update post methods for register and login

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,9 +1,8 @@
-import {
-  loginUser,
-  getCurrentUser,
-  registerUser,
-} from './../controllers/authController';
+import { get_currentUser } from './../controllers/userController';
+import { auth_login_post } from '../controllers/authController';
+import { post_register } from '../controllers/userController';
 import express, { Request, Response } from 'express';
+import passport from 'passport';
 
 const router = express.Router();
 
@@ -11,8 +10,12 @@ router.get('/', (req: Request, res: Response) => {
   res.json({ message: 'welcome to api' });
 });
 
-router.post('/register', registerUser);
-router.post('/login', loginUser);
-router.post('/me', getCurrentUser);
+router.post('/register', post_register);
+router.post('/login', auth_login_post);
+router.get(
+  '/me',
+  passport.authenticate('jwt', { session: false }),
+  get_currentUser
+);
 
 module.exports = router;
