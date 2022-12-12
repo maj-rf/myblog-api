43278a1e0ab468513d867bffb2a12738b644bab3
bananabread08 <rmkfermin@gmail.com>
2022-12-12 18:27:32 +0800

    make username required in schema

--- src/models/User.ts
+++ src/models/User.ts
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 
 const UserSchema = new Schema(
   {
-    username: { type: String, required: true, maxLength: 25 },
+    username: { type: String, required: true, maxLength: 25, unique: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true, minLength: 6 },
   },
