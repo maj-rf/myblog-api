15408238d8196854625f7df5915ebf0c024e511e
bananabread08 <rmkfermin@gmail.com>
2022-12-14 10:58:30 +0800

    complete all routes for CRUD methods

--- src/routes/postRoutes.ts
+++ src/routes/postRoutes.ts
@@ -1,16 +1,39 @@
 import express, { Router } from 'express';
-import { getAllPosts, createPost } from '../controllers/postController';
+import {
+  getAllPosts,
+  createPost,
+  getCurrentUserPosts,
+  deletePost,
+  updatePost,
+} from '../controllers/postController';
 import passport from 'passport';
 const postRouter = express.Router();
 
 // GET All posts
-postRouter.get('/', getAllPosts);
+postRouter.get('/all', getAllPosts);
+
+// GET All User posts
+postRouter.get('/all/:username', getCurrentUserPosts);
 
 // CREATE Post
 postRouter.post(
   '/',
   passport.authenticate('jwt', { session: false }),
   createPost
+);
+
+// DELETE Post
+postRouter.delete(
+  '/:id/delete',
+  passport.authenticate('jwt', { session: false }),
+  deletePost
+);
+
+// Update Post
+postRouter.put(
+  '/:id/update',
+  passport.authenticate('jwt', { session: false }),
+  updatePost
 );
 
 export default postRouter;
