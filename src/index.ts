74c2c37b2f84a94fe3e3186a6bf96e4656f0590e
bananabread08 <rmkfermin@gmail.com>
2022-12-15 09:35:45 +0800

    use commentRoutes

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import { init } from './config/mongoConfig';
 import { passportInit } from './config/passportConfig';
-import express from 'express';
+import express, { Response, NextFunction } from 'express';
 import passport from 'passport';
 import cors from 'cors';
 import indexRouter from './routes';
 import authRouter from './routes/authRoutes';
 import userRouter from './routes/userRoutes';
 import postRouter from './routes/postRoutes';
+import commentRouter from './routes/commentRoutes';
+import { ExtendedRequest } from './types/extReq';
 const app = express();
 
 //middlewares
@@ -22,3 +24,11 @@ app.use('/', indexRouter);
 app.use('/api', authRouter);
 app.use('/api/profile/', userRouter);
 app.use('/api/posts', postRouter);
+app.use(
+  '/api/posts/:id/comments/',
+  (req: ExtendedRequest, res: Response, next: NextFunction) => {
+    req.postId = req.params.id;
+    next();
+  },
+  commentRouter
+);
