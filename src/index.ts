65c3e72335ae4767d7ad5001978ccc602c37cb39
bananabread08 <rmkfermin@gmail.com>
2023-04-19 16:52:31 +0800

    initial commit for revisit

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,11 @@
-import { init } from './config/mongoConfig';
-import { passportInit } from './config/passportConfig';
-import express, { Response, NextFunction } from 'express';
-import passport from 'passport';
+import express from 'express';
 import cors from 'cors';
-import indexRouter from './routes';
-import authRouter from './routes/authRoutes';
-import userRouter from './routes/userRoutes';
-import postRouter from './routes/postRoutes';
-import commentRouter from './routes/commentRoutes';
-import { ExtendedRequest } from './types/extReq';
 const app = express();
+const PORT = 3003;
 
-//middlewares
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 app.use(cors());
-//mongoDB & passport
-init(app);
-passportInit(passport);
-app.use(passport.initialize());
+app.use(express.json());
 
-app.use('/', indexRouter);
-app.use('/api', authRouter);
-app.use('/api/profile/', userRouter);
-app.use('/api/posts', postRouter);
-app.use(
-  '/api/posts/:id/comments/',
-  (req: ExtendedRequest, res: Response, next: NextFunction) => {
-    req.postId = req.params.id;
-    next();
-  },
-  commentRouter
-);
+app.listen(PORT, () => {
+  console.log(`Server running at PORT: ${PORT}`);
+});
