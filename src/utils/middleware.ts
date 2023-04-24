a40c1e78b4b2eb5e7572bff417f805ba8bf74e1c
bananabread08 <rmkfermin@gmail.com>
2023-04-24 15:50:27 +0800

    remove ValidationError condition from auth

--- src/utils/middleware.ts
+++ src/utils/middleware.ts
@@ -15,9 +15,11 @@ export const errorHandler: ErrorRequestHandler = (
 ) => {
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' });
-  } else if (error.name === 'ValidationError') {
-    return response.status(400).json({ error: error.message });
-  } else if (error.name === 'JsonWebTokenError') {
+  }
+  // else if (error.name === 'ValidationError') {
+  //   return response.status(400).json({ error: error.message });
+  // }
+  else if (error.name === 'JsonWebTokenError') {
     return response.status(400).json({ error: 'Unauthorized User' });
   } else if (error.name === 'TokenExpiredError') {
     return response.status(401).json({
