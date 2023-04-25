18b94b08437eee264cdc9142271d38d0c10041dc
bananabread08 <rmkfermin@gmail.com>
2023-04-25 14:44:25 +0800

    remove comments

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -55,37 +55,6 @@ export const registerUser = [
   },
 ];
 
-// export const registerUser = async (
-//   req: Request,
-//   res: Response,
-//   next: NextFunction,
-// ) => {
-//   const { username, email, password } = req.body;
-//   if (!username || !email || !password) {
-//     return res
-//       .status(400)
-//       .json({ error: 'Missing username, email, or password' });
-//   }
-//   if (username.length < 6 || password.length < 6) {
-//     return res.status(400).json({
-//       error: 'Username and Password must be at least 6 characters long.',
-//     });
-//   }
-//   try {
-//     const saltRounds = 10;
-//     const passwordHash = await bcrypt.hash(password, saltRounds);
-//     const user = new User({
-//       username,
-//       email,
-//       password: passwordHash,
-//     });
-//     const savedUser = await user.save();
-//     res.status(201).json(savedUser);
-//   } catch (error) {
-//     next(error);
-//   }
-// };
-
 export const getAllUsers = async (_req: Request, res: Response) => {
   const users = await User.find({}).populate('blog');
   res.json(users);
