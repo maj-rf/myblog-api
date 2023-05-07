c6a607adc068e349f511fd06512c1ac43400c91c
bananabread08 <rmkfermin@gmail.com>
2023-05-07 11:11:52 +0800

    create profile route

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -1,6 +1,8 @@
 import express from 'express';
-import { getAllUsers } from '../controllers/userController';
+import { getAllUsers, getCurrentUser } from '../controllers/userController';
 
 export const userRouter = express.Router();
 
 userRouter.get('/', getAllUsers);
+
+userRouter.get('/profile', getCurrentUser);
