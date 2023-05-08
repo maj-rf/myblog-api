613977717664e1540b3c65e98d3cd80d1bcd14c7
bananabread08 <rmkfermin@gmail.com>
2023-05-08 16:37:48 +0800

    add condition if env is in test

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,7 +1,9 @@
 export const info = (params: string | object) => {
+  if (process.env.NODE_ENV === 'test') return;
   console.log(params);
 };
 
 export const error = (params: string | object) => {
+  if (process.env.NODE_ENV === 'test') return;
   console.error(params);
 };
