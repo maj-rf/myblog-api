c45e3b2e99eeb5c2bb9f62b6569540710da2b2cd
bananabread08 <rmkfermin@gmail.com>
2023-06-05 11:28:50 +0800

    create recent comment route

--- src/routes/commentRouter.ts
+++ src/routes/commentRouter.ts
@@ -2,10 +2,13 @@ import express from 'express';
 import {
   createCommentForThisBlog,
   getAllCommentsForThisBlog,
+  getRecentComments,
 } from '../controllers/commentController';
 import { verifyJWT } from '../middlewares/middleware';
 
 export const commentRouter = express.Router();
+
+commentRouter.get('/recent', getRecentComments);
 
 commentRouter.get('/:id', getAllCommentsForThisBlog);
 
